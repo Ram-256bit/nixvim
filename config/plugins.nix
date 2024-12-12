8d44c626d1664bc972e8c48766bbef60b9d7526a
Ram-256bit <72127084+Ram-256bit@users.noreply.github.com>
2024-12-12 15:21:40 +0530

    1

---
+++ config/plugins.nix
@@ -0,0 +1,24 @@
+{ 
+  colorschemes = {
+    catppuccin = {
+      enable = true;
+    };
+  };
+
+  plugins = {
+    bufferline.enable = true;
+    web-devicons.enable = true;
+    nvim-autopairs = {
+    enable = true;
+    settings = {
+      disable_filetype = [
+        "TelescopePrompt"
+        "vim"
+      ];
+    };
+  };
+   
+
+
+  };
+}
